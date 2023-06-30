b2a2a54ca6055ffa8c8556a78f724ff01dfff1b0
ella hanzin <hanzin@g.jct.ac.il>
2023-06-30 13:35:57 +0300

    changed

--- behavior_func/All_Plots.m
+++ behavior_func/All_Plots.m
@@ -4,149 +4,145 @@ function [] = All_Plots(data)
 
 f1=figure;
 set(f1, 'color', [1 1 1]);
-set(f1,'position',[300 50 900 700]);
+set(f1,'position',[150 50 1300 700]);
 set(f1,'name',"All_Tests");
 %% Suger pref
-subplot(4, 2, 1);
+subplot(2, 4, 1);
 hold on
-plot(ones(size(data.CT13.SP.Right(1,:) ,1),1),data.CT13.SP.Right(1,:),'ro','MarkerSize',10);
-plot(2.*ones(size(data.T24.SP.Right(1,:),1),1),data.T24.SP.Right(1,:),'bo','MarkerSize',10);
+plot(ones(size(data.CT13.SP.Right(1,:) ,1),1),data.CT13.SP.Right(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#A2142F');
+plot(2.*ones(size(data.T24.SP.Right(1,:),1),1),data.T24.SP.Right(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#0072BD');
 xlim([0 3]);
 xticks([1 2])
+ylim([0 1]);
 xticklabels({'CT13','T24'})
 xlabel('cycle')
 ylabel('consumed/total consumed(%) ')
-errorbar(1.1,mean(data.CT13.SP.Right(1,:)),std(data.CT13.SP.Right(1,:)),'ro','MarkerSize',10,'LineWidth',2);
-errorbar(2.1,mean(data.T24.SP.Right(1,:)),std(data.T24.SP.Right(1,:)),'bo','MarkerSize',10,'LineWidth',2);
-title('sugar preference test-Right')
-% Permutation t-test
-% [p, observeddifference, effectsize]=permutationTest(data.CT13.SP.Right(1,:),data.T24.SP.Right(1,:),10000)  ;
-% str={'p=' p 'observeddifference=' observeddifference 'effectsize=' effectsize};
-% annotation('textbox',[.7 .5 .1 .2],'String',str,'FitBoxToText','on');
+e1=errorbar(1.1,mean(data.CT13.SP.Right(1,:)),std(data.CT13.SP.Right(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#A2142F');
+e1.Color='#A2142F';
+e2=errorbar(2.1,mean(data.T24.SP.Right(1,:)),std(data.T24.SP.Right(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#0072BD');
+e2.Color='#0072BD';
+title('Right side sugar preference test','FontSize',14)
 
-subplot(4, 2, 2);
+subplot(2, 4, 5);
 hold on
-plot(ones(size(data.CT13.SP.Left(1,:) ,1),1),data.CT13.SP.Left(1,:),'ro','MarkerSize',10);
-plot(2.*ones(size(data.T24.SP.Left(1,:),1),1),data.T24.SP.Left(1,:),'bo','MarkerSize',10);
+plot(ones(size(data.CT13.SP.Left(1,:) ,1),1),data.CT13.SP.Left(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#A2142F');
+plot(2.*ones(size(data.T24.SP.Left(1,:),1),1),data.T24.SP.Left(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#0072BD');
 xlim([0 3]);
 xticks([1 2])
+ylim([0 1]);
 xticklabels({'CT13','T24'})
 xlabel('cycle')
 ylabel('consumed/total consumed(%) ')
-errorbar(1.1,mean(data.CT13.SP.Left(1,:)),std(data.CT13.SP.Left(1,:)),'ro','MarkerSize',10,'LineWidth',2);
-errorbar(2.1,mean(data.T24.SP.Left(1,:)),std(data.T24.SP.Left(1,:)),'bo','MarkerSize',10,'LineWidth',2);
-title('sugar preference test-Left')
-% Permutation t-test
-% [p, observeddifference, effectsize]=permutationTest(data.CT13.SP.Left(1,:),data.T24.SP.Left(1,:),10000);
-% str={'p=' p 'observeddifference=' observeddifference 'effectsize=' effectsize};
-% annotation('textbox',[.7 .5 .1 .2],'String',str,'FitBoxToText','on');
+e1 =errorbar(1.1,mean(data.CT13.SP.Left(1,:)),std(data.CT13.SP.Left(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#A2142F');
+e1.Color='#A2142F';
+e2=errorbar(2.1,mean(data.T24.SP.Left(1,:)),std(data.T24.SP.Left(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#0072BD');
+e2.Color='#0072BD';
+title('Left side sugar preference test','FontSize',14)
 
 %% Open Feild test plots
-subplot(4, 2, 3);
+subplot(2, 4, 3);
 hold on
-plot(ones(size(data.CT13.OF.duration(1,:) ,1),1),data.CT13.OF.duration(1,:),'ro','MarkerSize',10);
-plot(2.*ones(size(data.T24.OF.duration(1,:),1),1),data.T24.OF.duration(1,:),'bo','MarkerSize',10);
+plot(ones(size(data.CT13.OF.duration(1,:) ,1),1),data.CT13.OF.duration(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#A2142F');
+plot(2.*ones(size(data.T24.OF.duration(1,:),1),1),data.T24.OF.duration(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#0072BD');
 xlim([0 3]);
 xticks([1 2])
+ylim([0 1]);
 xticklabels({'CT13','T24'})
 xlabel('cycle')
 ylabel('Center/total time(%) ')
-errorbar(1.1,mean(data.CT13.OF.duration(1,:)),std(data.CT13.OF.duration(1,:)),'ro','MarkerSize',10,'LineWidth',2);
-errorbar(2.1,mean(data.T24.OF.duration(1,:)),std(data.T24.OF.duration(1,:)),'bo','MarkerSize',10,'LineWidth',2);
-title('OF Duration CT13 VS T24')
- % Permutation t-test
-% [p, observeddifference, effectsize]=permutationTest(data.CT13.OF.duration(1,:),data.T24.OF.duration(1,:),10000); 
-% str={'p=' p 'observeddifference=' observeddifference 'effectsize=' effectsize};
-% annotation('textbox',[.7 .5 .1 .2],'String',str,'FitBoxToText','on');
+e1=errorbar(1.1,mean(data.CT13.OF.duration(1,:)),std(data.CT13.OF.duration(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#A2142F');
+e1.Color='#A2142F';
+e2=errorbar(2.1,mean(data.T24.OF.duration(1,:)),std(data.T24.OF.duration(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#0072BD');
+e2.Color='#0072BD';
+title('Duration OFT CT13 VS T24','FontSize',14)
 
-subplot(4, 2, 4);
+
+subplot(2, 4, 7);
 hold on
-plot(ones(size(data.CT13.OF.distance(1,:) ,1),1),data.CT13.OF.distance(1,:),'ro','MarkerSize',10);
-plot(2.*ones(size(data.T24.OF.distance(1,:),1),1),data.T24.OF.distance(1,:),'bo','MarkerSize',10);
+plot(ones(size(data.CT13.OF.distance(1,:) ,1),1),data.CT13.OF.distance(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#A2142F');
+plot(2.*ones(size(data.T24.OF.distance(1,:),1),1),data.T24.OF.distance(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#0072BD');
 xlim([0 3]);
 xticks([1 2])
+ylim([0 1]);
 xticklabels({'CT13','T24'})
 xlabel('cycle')
 ylabel('Periphry/total time(%) ')
-errorbar(1.1,mean(data.CT13.OF.distance(1,:)),std(data.CT13.OF.distance(1,:)),'ro','MarkerSize',10,'LineWidth',2);
-errorbar(2.1,mean(data.T24.OF.distance(1,:)),std(data.T24.OF.distance(1,:)),'bo','MarkerSize',10,'LineWidth',2);
-title('OF Distance CT13 VS T24')
-% Permutation t-test
-% [p, observeddifference, effectsize]=permutationTest(data.CT13.OF.distance(1,:),data.T24.OF.distance(1,:),10000);  
-% str={'p=' p 'observeddifference=' observeddifference 'effectsize=' effectsize};
-% annotation('textbox',[.7 .5 .1 .2],'String',str,'FitBoxToText','on');
-hold off
+e1=errorbar(1.1,mean(data.CT13.OF.distance(1,:)),std(data.CT13.OF.distance(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#A2142F');
+e1.Color='#A2142F';
+e2=errorbar(2.1,mean(data.T24.OF.distance(1,:)),std(data.T24.OF.distance(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#0072BD');
+e2.Color='#0072BD';
+title('Distance OFT CT13 VS T24','FontSize',14)
+
 %% EPM Test
-subplot(4, 2, 5);
+subplot(2, 4, 2);
 hold on
-plot(ones(size(data.CT13.EPM.duration(1,:) ,1),1),data.CT13.EPM.duration(1,:),'ro','MarkerSize',10);
-plot(2.*ones(size(data.T24.EPM.duration(1,:),1),1),data.T24.EPM.duration(1,:),'bo','MarkerSize',10);
+plot(ones(size(data.CT13.EPM.duration(1,:) ,1),1),data.CT13.EPM.duration(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#A2142F');
+plot(2.*ones(size(data.T24.EPM.duration(1,:),1),1),data.T24.EPM.duration(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#0072BD');
 xlim([0 3]);
 xticks([1 2])
+ylim([0 1]);
 xticklabels({'CT13','T24'})
 xlabel('cycle')
 ylabel('openHand/total duration(%) ')
-errorbar(1.1,mean(data.CT13.EPM.duration(1,:)),std(data.CT13.EPM.duration(1,:)),'ro','MarkerSize',10,'LineWidth',2);
-errorbar(2.1,mean(data.T24.EPM.duration(1,:)),std(data.T24.EPM.duration(1,:)),'bo','MarkerSize',10,'LineWidth',2);
-title('EPM Duration CT13 VS T24')
-% Permutation t-test
-% [p, observeddifference, effectsize]=permutationTest(data.CT13.EPM.duration(1,:),data.T24.EPM.duration(1,:),10000); 
-% str={'p=' p 'observeddifference=' observeddifference 'effectsize=' effectsize};
-% annotation('textbox',[.7 .5 .1 .2],'String',str,'FitBoxToText','on');
-hold off
+e1=errorbar(1.1,mean(data.CT13.EPM.duration(1,:)),std(data.CT13.EPM.duration(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#A2142F');
+e1.Color='#A2142F';
+e2=errorbar(2.1,mean(data.T24.EPM.duration(1,:)),std(data.T24.EPM.duration(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#0072BD');
+e2.Color='#0072BD';
+title('Duration EPM CT13 VS T24','FontSize',14)
 
-subplot(4, 2, 6);
+
+subplot(2, 4, 6);
 hold on
-plot(ones(size(data.CT13.EPM.distance(1,:) ,1),1),data.CT13.EPM.distance(1,:),'ro','MarkerSize',10);
-plot(2.*ones(size(data.T24.EPM.distance(1,:),1),1),data.T24.EPM.distance(1,:),'bo','MarkerSize',10);
+plot(ones(size(data.CT13.EPM.distance(1,:) ,1),1),data.CT13.EPM.distance(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#A2142F');
+plot(2.*ones(size(data.T24.EPM.distance(1,:),1),1),data.T24.EPM.distance(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#0072BD');
 xlim([0 3]);
 xticks([1 2])
+ylim([0 1]);
 xticklabels({'CT13','T24'})
 xlabel('cycle')
 ylabel('openHand/total distance(%) ')
-errorbar(1.1,mean(data.CT13.EPM.distance(1,:)),std(data.CT13.EPM.distance(1,:)),'ro','MarkerSize',10,'LineWidth',2);
-errorbar(2.1,mean(data.T24.EPM.distance(1,:)),std(data.T24.EPM.distance(1,:)),'bo','MarkerSize',10,'LineWidth',2);
-title('EPM Distance CT13 VS T24')
-% Permutation t-test
-% [p, observeddifference, effectsize]=permutationTest(data.CT13.EPM.distance(1,:),data.T24.EPM.distance(1,:),10000); 
-% str={'p=' p 'observeddifference=' observeddifference 'effectsize=' effectsize};
-% annotation('textbox',[.7 .5 .1 .2],'String',str,'FitBoxToText','on');
-hold off
+e1=errorbar(1.1,mean(data.CT13.EPM.distance(1,:)),std(data.CT13.EPM.distance(1,:))...
+    ,'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#A2142F');
+e1.Color='#A2142F';
+e2=errorbar(2.1,mean(data.T24.EPM.distance(1,:)),std(data.T24.EPM.distance(1,:))...
+    ,'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#0072BD');
+e2.Color='#0072BD';
+title('Distance EPM CT13 VS T24','FontSize',14)
+
 %% forse swiming test
 
-subplot(4, 2, 7);
+subplot(2, 4, 4);
 hold on
-plot(ones(size(data.CT13.FS.duration(1,:) ,1),1),data.CT13.FS.duration(1,:),'ro','MarkerSize',10);
-plot(2.*ones(size(data.T24.FS.duration(1,:),1),1),data.T24.FS.duration(1,:),'bo','MarkerSize',10);
+plot(ones(size(data.CT13.FS.duration(1,:) ,1),1),data.CT13.FS.duration(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#A2142F');
+plot(2.*ones(size(data.T24.FS.duration(1,:),1),1),data.T24.FS.duration(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#0072BD');
 xlim([0 3]);
 xticks([1 2])
+ylim([0 1]);
 xticklabels({'CT13','T24'})
 xlabel('cycle')
 ylabel('inactive/active+inactive(%) ')
-errorbar(1.1,mean(data.CT13.FS.duration(1,:)),std(data.CT13.FS.duration(1,:)),'ro','MarkerSize',10,'LineWidth',2);
-errorbar(2.1,mean(data.T24.FS.duration(1,:)),std(data.T24.FS.duration(1,:)),'bo','MarkerSize',10,'LineWidth',2);
-title('FST Duration CT13 VS T24')
-% Permutation t-test
-% [p, observeddifference, effectsize]=permutationTest(data.CT13.FS.duration(1,:),data.T24.FS.duration(1,:),10000) ; 
-% str={'p=' p 'observeddifference=' observeddifference 'effectsize=' effectsize};
-% annotation('textbox',[.7 .5 .1 .2],'String',str,'FitBoxToText','on');
-hold off
+e1=errorbar(1.1,mean(data.CT13.FS.duration(1,:)),std(data.CT13.FS.duration(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#A2142F');
+e1.Color='#A2142F';
+e2=errorbar(2.1,mean(data.T24.FS.duration(1,:)),std(data.T24.FS.duration(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#0072BD');
+e2.Color='#0072BD';
+title('Duration FST CT13 VS T24','FontSize',14)
 
-subplot(4, 2, 8);
+subplot(2, 4, 8);
 hold on
-plot(ones(size(data.CT13.FS.frequency(1,:) ,1),1),data.CT13.FS.frequency(1,:),'ro','MarkerSize',10);
-plot(2.*ones(size(data.T24.FS.frequency(1,:),1),1),data.T24.FS.frequency(1,:),'bo','MarkerSize',10);
+plot(ones(size(data.CT13.FS.frequency(1,:) ,1),1),data.CT13.FS.frequency(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#A2142F');
+plot(2.*ones(size(data.T24.FS.frequency(1,:),1),1),data.T24.FS.frequency(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#0072BD');
 xlim([0 3]);
 xticks([1 2])
+ylim([0 1]);
 xticklabels({'CT13','T24'})
 xlabel('cycle')
 ylabel('inactive/active+inactive(%) ')
-errorbar(1.1,mean(data.CT13.FS.frequency(1,:)),std(data.CT13.FS.frequency(1,:)),'ro','MarkerSize',10,'LineWidth',2);
-errorbar(2.1,mean(data.T24.FS.frequency(1,:)),std(data.T24.FS.frequency(1,:)),'bo','MarkerSize',10,'LineWidth',2);
-title('FST Frequency CT13 VS T24')
-% Permutation t-test
-% [p, observeddifference, effectsize]=permutationTest(data.CT13.FS.frequency(1,:),data.T24.FS.frequency(1,:),10000);  
-% str={'p=' p 'observeddifference=' observeddifference 'effectsize=' effectsize};
-% annotation('textbox',[.7 .5 .1 .2],'String',str,'FitBoxToText','on');
+e1=errorbar(1.1,mean(data.CT13.FS.frequency(1,:)),std(data.CT13.FS.frequency(1,:))...
+    ,'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#A2142F');
+e1.Color='#A2142F';
+e2=errorbar(2.1,mean(data.T24.FS.frequency(1,:)),std(data.T24.FS.frequency(1,:))...
+    ,'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#0072BD');
+e2.Color='#0072BD';
+title('Frequency FST CT13 VS T24','FontSize',14)
 hold off
 end
--- behavior_func/FST_Plot.m
+++ behavior_func/FST_Plot.m
@@ -5,8 +5,6 @@ set(f1,'position',[300 50 700 500]);%['start from right','start from bottom','fi
 set(f1,'name',"FST_Plot");
 subplot(1,2,1)
 hold on
-newcolors = {'#A2142F','#0072BD'};
-colororder(newcolors)
 plot(ones(size(data.CT13.FS.duration(1,:) ,1),1),data.CT13.FS.duration(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#A2142F');
 plot(2.*ones(size(data.T24.FS.duration(1,:),1),1),data.T24.FS.duration(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#0072BD');
 xlim([0 3]);
@@ -15,8 +13,10 @@ ylim([0 1]);
 xticklabels({'CT13','T24'})
 xlabel('cycle')
 ylabel('duration in open hand/total duration')
-errorbar(1.1,mean(data.CT13.FS.duration(1,:)),std(data.CT13.FS.duration(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#A2142F');
-errorbar(2.1,mean(data.T24.FS.duration(1,:)),std(data.T24.FS.duration(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#0072BD');
+e1=errorbar(1.1,mean(data.CT13.FS.duration(1,:)),std(data.CT13.FS.duration(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#A2142F');
+e1.Color='#A2142F';
+e2=errorbar(2.1,mean(data.T24.FS.duration(1,:)),std(data.T24.FS.duration(1,:)),'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#0072BD');
+e2.Color='#0072BD';
 title('Duration FST CT13 VS T24','FontSize',14)
 % Permutation t-test
 [p, observeddifference, effectsize]=permutationTest(data.CT13.FS.duration(1,:),data.T24.FS.duration(1,:),10000); 
@@ -27,8 +27,6 @@ subtitle(s,"FontSize",9)
 %%
 subplot(1,2,2)
 hold on
-newcolors = {'#A2142F','#0072BD'};
-colororder(newcolors)
 plot(ones(size(data.CT13.FS.frequency(1,:) ,1),1),data.CT13.FS.frequency(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#A2142F');
 plot(2.*ones(size(data.T24.FS.frequency(1,:),1),1),data.T24.FS.frequency(1,:),'o','MarkerSize',10,'MarkerEdgeColor','#0072BD');
 xlim([0 3]);
@@ -37,10 +35,12 @@ ylim([0 1]);
 xticklabels({'CT13','T24'})
 xlabel('cycle')
 ylabel('distance in open hand/total distance')
-errorbar(1.1,mean(data.CT13.FS.frequency(1,:)),std(data.CT13.FS.frequency(1,:))...
+e1=errorbar(1.1,mean(data.CT13.FS.frequency(1,:)),std(data.CT13.FS.frequency(1,:))...
     ,'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#A2142F');
-errorbar(2.1,mean(data.T24.FS.frequency(1,:)),std(data.T24.FS.frequency(1,:))...
+e1.Color='#A2142F';
+e2=errorbar(2.1,mean(data.T24.FS.frequency(1,:)),std(data.T24.FS.frequency(1,:))...
     ,'o','MarkerSize',10,'LineWidth',2,'MarkerEdgeColor','#0072BD');
+e2.Color='#0072BD';
 title('Frequency FST CT13 VS T24','FontSize',14)
 % Permutation t-test
 [p, observeddifference, effectsize]=permutationTest(data.CT13.FS.frequency(1,:),data.T24.FS.frequency(1,:),10000); 
