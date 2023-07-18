02e21cd928e260b1cfd618eb9bb8132b541b006d
ella hanzin <hanzin@g.jct.ac.il>
2023-07-18 16:34:33 +0300

    update

--- Infra_Red_func/differencesInActivity.m
+++ Infra_Red_func/differencesInActivity.m
@@ -10,7 +10,7 @@ set(f7,'color', [1 1 1]);
 set(f7,'position',[20 50 300 400]);
 hold on;
 plot([size(AdataMT{1},2)+0.5 size(AdataMT{1},2)+0.5],[0 30],'--','Color','k');
-plot(1:(size(AdataMT{1},2)+size(AdataMT{2},2)),[AdataMT{1},AdataMT{2}],'o','LineWidth',2,'Color','b','MarkerSize',10);
+plot(1:(size(AdataMT{1},2)+size(AdataMT{2},2)),[AdataMT{1},AdataMT{2}],'o','LineWidth',2,'Color','#0072BD','MarkerSize',10);
 ylabel('Mean absolute activity (a.u.)','FontSize',18);
 xlabel('Mouse #','FontSize',18);
 box off
